82a2619979ec1ad813bdeac6b6d5485d895fe0ed
tomo-ut <>
2020-10-25 03:43:03 +0900

    finvのtableの作成、pythonでのエミュレート

---
+++ 1st/fpu/finv.py
@@ -0,0 +1,41 @@
+with open('finv_table.mem', 'r') as infile:
+    table = infile.read().splitlines()
+
+for i, line in enumerate(table):
+    table[i] = line.split()
+    table[i] = [int(table[i][j], 2) for j in range(3)]
+
+with open('finv_input.txt', 'r') as infile:
+    indata = infile.read().splitlines()
+
+for i, line in enumerate(indata):
+    indata[i] = [int(line[:10], 2), int(line[10:], 2)]
+
+with open('finv_output.txt', 'w') as outfile:
+    for line in indata:
+        idx = line[0]
+        c = table[idx][1]
+        d = table[idx][2]
+        if idx <= 423:
+            ans = c - ((line[1] * d) >> 13)
+        elif idx <= 1022:
+            ans = c - ((line[1] * d) >> 14)
+        else:
+            ans = c - ((line[1] * d) >> 15)
+        outfile.write(f'{ans:023b}\n')
+
+
+# with open('finv_table_bin.mem', 'r') as infile:
+#     table = infile.read().splitlines()
+
+# for i, line in enumerate(table):
+#     table[i] = line.split()
+#     table[i] = [table[i][j] for j in range(3)]
+
+# new_table = [table[i][1] + table[i][2] for i in range(len(table))]
+# with open('finv_table_hex.mem', 'w') as outfile:
+#     for s_value in new_table:
+#         val = int(s_value, 2)
+#         outfile.write(f'{val:09X}\n')
+
+# %%
